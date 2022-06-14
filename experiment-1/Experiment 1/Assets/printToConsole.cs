e63db655105d10f75e364d008460c649d5aa3a5f
zm-dksg <zemariadekonincksg@gmail.com>
2022-06-14 19:15:25 +0100

    project done in the first chapter of the course (first commit)
    project is very simple, just creating some 3D objects and prefabs

---
+++ experiment-1/Experiment 1/Assets/printToConsole.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printToConsole : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("I am a TANK!!");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
